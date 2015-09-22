f6bb9efb2aee0bacdf81d9cbe0c52ca1691dbfa7
jbenkual <joe@benkual.com>
2015-09-21 22:08:05 -0700

    Add jade layout to display task list

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', './routes/index');
-app.use('/notes', './routes/notes');
-app.use('/users', './routes/users');
+app.use('/', require('./routes/index'));
+app.use('/notes', require('./routes/notes'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
